ec14d6eef1bcfc9606fd6be03a6c8dc25710198e
Takashi Saito <takashi11171117@gmail.com>
2021-04-16 17:16:20 +0900

    add global style and normalize

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,31 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { createGlobalStyle } from "styled-components"
+import { normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+${normalize}
+* {
+  text-decoration: none;
+  cursor: none;
+}
+html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  
+  
+}
+body {
+  font-size: 16px;
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background: #ffffff;
+  overscroll-behavior: none;
+  overflow-x: hidden;
+}
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +38,12 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  return (
+    <>
+      <GlobalStyle />
+      <main>{children}</main>
+    </>
+  )
 }
 
 Layout.propTypes = {
